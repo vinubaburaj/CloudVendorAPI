300ce40764e4f58b578eab81a23f07d0c327e91e
Vinu Baburaj <vinu@Vinus-MacBook-Air.local>
2023-05-07 23:23:40 -0400

    Repository and Service Layer tests

--- src/main/java/com/spring/restdemo/repository/CloudVendorRepository.java
+++ src/main/java/com/spring/restdemo/repository/CloudVendorRepository.java
@@ -3,5 +3,8 @@ package com.spring.restdemo.repository;
 import com.spring.restdemo.model.CloudVendor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CloudVendorRepository extends JpaRepository<CloudVendor, String> {
+    List<CloudVendor> findByVendorName(String vendorName);
 }
